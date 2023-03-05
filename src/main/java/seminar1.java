983df40b0373b67d823a8abe26ec6eb74660b4a2
BorAlex <alexander44@bk.ru>
2023-03-05 14:10:25 +0300

    update seminar1 task1

--- src/main/java/seminar1.java
+++ src/main/java/seminar1.java
@@ -1,10 +1,51 @@
 import java.time.LocalTime;
+import java.util.Random;
 import java.util.Scanner;
 
 public class seminar1 {
     public static void main(String[] args) {
-        inputNameAndPrintHi();
+        //inputNameAndPrintHi();
+        findMaxAndMinElemArray();
 
+    }
+
+    private static void findMaxAndMinElemArray() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите длину массива: ");
+        int num = in.nextInt();
+        Random rand = new Random();
+        int[] array;
+        array = new int[num];
+        for (int i = 0; i < num; i++) {
+            array[i] = rand.nextInt(100);
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        int max = getMax(array);
+        System.out.println("Максимальное значение массива: " + max);
+        int min = getMin(array);
+        System.out.println("Минимальное значение массива: "+min);
+
+    }
+
+    public static int getMax(int[] inputArray) {
+        int maxValue = inputArray[0];
+        for (int i = 1; i < inputArray.length; i++) {
+            if (inputArray[i] > maxValue) {
+                maxValue = inputArray[i];
+            }
+        }
+        return maxValue;
+    }
+
+    public static int getMin(int[] inputArray) {
+        int minValue = inputArray[0];
+        for (int i = 1; i < inputArray.length; i++) {
+            if (inputArray[i] < minValue) {
+                minValue = inputArray[i];
+            }
+        }
+        return minValue;
     }
 
     private static void inputNameAndPrintHi() {
